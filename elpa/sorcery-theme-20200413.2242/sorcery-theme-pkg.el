ce7a4bd4860aa37b7e5f9705ebde164933e84235
Artour <suiko@airmail.cc>
2020-05-07 12:06:23 -0500

    removed centaur tabs

---
+++ elpa/sorcery-theme-20200413.2242/sorcery-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "sorcery-theme" "20200413.2242" "A D&D (Dark and Dusty) Theme" '((autothemer "0.2")) :commit "fb041d0d8d4ae2840ddcfb1bd56fefe687da8c52" :authors '(("Maxime Tréca" . "maxime@gmail.com")) :maintainer '("Maxime Tréca" . "maxime@gmail.com") :url "http://github.com/vxid/emacs-theme-sorcery")
